55ea306e136f1bba9239ba59ace0ea0e9ba0db85
Joel Brigida <joel@drtechreps.com>
2023-06-15 21:12:24 -0400

    User Input Keypad Prints To Terminal

---
+++ test_progs/Gen-1-MSP430/Keypad_test/Keypad_test.cpp
@@ -0,0 +1,43 @@
+/**
+ * Joel Brigida
+ * CDA-4630 Embedded Systems
+ * This is a test program I used to verify user keypad input to the
+ * MCU. The keypad is scanned continuously and any key press is printed
+ * to the terminal.
+*/
+
+#include <Key.h>
+#include <Keypad.h>
+#include <driverlib.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char buttons[ROWS][COLS] = {                              // declare keypad button as 2-D array
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {P1_0, P2_6, P2_7, P1_7};            // declare pins of keypad as byte arrays
+byte colPins[COLS] = {P1_6, P2_5, P2_4, P2_3};
+
+// declare and initialize Keypad Object
+Keypad the_keypad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
+
+void setup()
+{
+    Serial.begin(4800);
+    Serial.println("This is a test");
+}
+
+void loop()
+{
+    char myKeyPress = the_keypad.getKey();
+
+    if (myKeyPress)
+    {
+        Serial.println(myKeyPress);
+    }
+}
